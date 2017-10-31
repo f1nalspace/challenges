ce92407675426a6456501e68548fa432bd9dd6e6
Finalspace <finalspace@gmail.com>
2017-10-31 15:20:19 +0100

    Bresenham with width

--- gd_challenge_oct_2017/game.cpp
+++ gd_challenge_oct_2017/game.cpp
@@ -879,14 +879,14 @@ namespace fs {
 			void Game::HandleInput(const Input &input) {
 				renderer->Update(HalfGameWidth, HalfGameHeight, GameAspect);
 
-				if (isEditor) {
-					EditorUpdate();
-				}
-
 				// Update world mouse position
 				const s32 mouseX = input.mouse.pos.x;
 				const s32 mouseY = renderer->windowSize.h - 1 - input.mouse.pos.y;
 				mouseWorldPos = renderer->Unproject(Vec2i(mouseX, mouseY));
+
+				if (isEditor) {
+					EditorUpdate();
+				}
 
 				if (input.keyboard.editorToggle.WasPressed()) {
 					if (isEditor) {
@@ -924,7 +924,19 @@ namespace fs {
 #endif
 			}
 
-			static void BresenhamLine(int x0, int y0, int x1, int y1, std::vector<Vec2i> &out) {
+			static bool Contains(const Vec2i &value, const std::vector<Vec2i> &list) {
+				bool result = false;
+				for (const Vec2i &test : list) {
+					if (IsEqual(value, test)) {
+						result = true;
+						break;
+					}
+				}
+				return(result);
+			}
+
+			static void BresenhamLine(int x0, int y0, int x1, int y1, std::vector<Vec2i> &out, float wd) {
+#if 0
 				// https://de.wikipedia.org/wiki/Bresenham-Algorithmus
 				int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
 				int dy = -abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
@@ -936,6 +948,30 @@ namespace fs {
 					if (e2 > dy) { err += dy; x0 += sx; } /* e_xy+e_x > 0 */
 					if (e2 < dx) { err += dx; y0 += sy; } /* e_xy+e_y < 0 */
 				}
+#else
+				int dx = Absolute(x1 - x0), sx = x0 < x1 ? 1 : -1;
+				int dy = Absolute(y1 - y0), sy = y0 < y1 ? 1 : -1;
+				int err = dx - dy, e2, x2, y2;
+				float ed = dx + dy == 0 ? 1 : sqrt((float)dx*dx + (float)dy*dy);
+				for (wd = (wd + 1) / 2; ; ) {
+					out.emplace_back(Vec2i(x0, y0));
+					e2 = err; x2 = x0;
+					// x step
+					if (2 * e2 >= -dx) {
+						for (e2 += dy, y2 = y0; e2 < ed*wd && (y1 != y2 || dx > dy); e2 += dx)
+							out.emplace_back(Vec2i(x0, y2 += sy));
+						if (x0 == x1) break;
+						e2 = err; err -= dy; x0 += sx;
+					}
+					// y step
+					if (2 * e2 <= dy) {
+						for (e2 = dx - e2; e2 < ed*wd && (x1 != x2 || dx < dy); e2 += dy)
+							out.emplace_back(Vec2i(x2 += sx, y0));
+						if (y0 == y1) break;
+						err += dx; y0 += sy;
+					}
+				}
+#endif
 			}
 
 			void Game::Render() {
@@ -1002,30 +1038,17 @@ namespace fs {
 
 				Vec2f perp = Cross(1.0f, rayDirection);
 
-				const Vec2i offsets[] = {
-					Vec2i(0, 0), // No offset,
-					Vec2i(0, 1), // Up
-					Vec2i(-1, 1), // Left-up
-					Vec2i(-1, 0), // Left
-					Vec2i(-1, -1), // Left-down
-					Vec2i(0, -1), // Down
-					Vec2i(1, -1), // Right-Down
-					Vec2i(1, 0), // Right
-					Vec2i(1, 1), // Right-up
-				};
-
 				std::vector<Vec2i> lineTiles;
-				for (Vec2i offset : offsets) {
-					Vec2i rayStartTilePos = WorldToTile(rayStart) + offset;
-					Vec2i rayEndTilePos = WorldToTile(rayEnd) + offset;
+				Vec2i rayStartTilePos = WorldToTile(rayStart);
+				Vec2i rayEndTilePos = WorldToTile(rayEnd);
 
-					int x0 = rayStartTilePos.x;
-					int x1 = rayEndTilePos.x;
-					int y0 = rayStartTilePos.y;
-					int y1 = rayEndTilePos.y;
+				int x0 = rayStartTilePos.x;
+				int x1 = rayEndTilePos.x;
+				int y0 = rayStartTilePos.y;
+				int y1 = rayEndTilePos.y;
 
-					BresenhamLine(x0, y0, x1, y1, lineTiles);
-				}
+				BresenhamLine(x0, y0, x1, y1, lineTiles, 1.0f);
+
 				for (const Vec2i &lineTile : lineTiles) {
 					renderer->DrawRectangle(TileToWorld(lineTile), tileExt, Vec4f(1.0f, 0.0f, 1.0f, 0.5f));
 				}
